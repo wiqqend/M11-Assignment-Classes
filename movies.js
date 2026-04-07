7d3b4982dd3436177f19a2a6495b71a9ec8a0d46
Jacob <jacobdhartzell@gmail.com>
2026-04-07 12:50:09 -0400

    initialize

---
+++ movies.js
@@ -0,0 +1,16 @@
+// Define the Movie class
+
+  // title, cast, description, rating
+
+  // Method to update the rating of the movie
+
+  // Method to display the movie's information
+
+
+// Test your implementation
+
+// Display the initial information of the movie
+
+// Update the rating of the movie
+
+// Display the updated information of the movie
